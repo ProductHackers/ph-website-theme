367e978f8f11d4ddd8c22523b4edad19a7de506f
jorchg <rushcentro@gmail.com>
2018-08-28 14:42:01 +0200

    More cool accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,15 +1,28 @@
 (function() {
   window.addEventListener('load', function() {
     var accordionClickable = document.querySelectorAll('.context__paragraph h2.highlighted');
+    var contextSection = document.querySelector('section.h-section__context');
+    var panels = contextSection.querySelectorAll('.panel');
+
     accordionClickable.forEach(function(item) {
       item.addEventListener('click', function(e) {
+        accordionClickable.forEach(function(acc) {
+          if (acc !== item) acc.parentNode.classList.remove('expand');
+        });
+
+        panels.forEach(function(panel) {
+          panel.style.maxHeight = null;
+        });
+
         item.parentNode.classList.toggle('expand');
+
         var panel = item.parentNode.querySelector('.panel');
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
         } else {
           panel.style.maxHeight = `${panel.scrollHeight}px`;
         }
+
       });
     });
   });
