7fe3d00a1542643c0f08932f19988a0a9916669d
jorchg <rushcentro@gmail.com>
2018-08-29 15:54:03 +0200

    Scroll reveal truly mobile

--- assets/js/team_mobile_reveal.js
+++ assets/js/team_mobile_reveal.js
@@ -1,19 +1,20 @@
 (function() {
   window.addEventListener('load', function() {
-    var teamContainer = document.querySelector('.team__wrapper');
-    var teamCards = document.querySelectorAll('.m8');
+    if (window.innerWidth < 800) {
+      var teamContainer = document.querySelector('.team__wrapper');
+      var teamCards = document.querySelectorAll('.m8');
 
-    teamCards.forEach(function(card) {
-      var cardsWatcher = scrollMonitor.create(card, 70);
-      cardsWatcher.fullyEnterViewport(function(event, element) {
-        element.watchItem.querySelector('.overlay').style.opacity = 1;
-        element.watchItem.querySelector('.overlay').style.backgroundColor = 'rgba(249, 35, 0, 0.28)';
+      teamCards.forEach(function(card) {
+        var cardsWatcher = scrollMonitor.create(card, 70);
+        cardsWatcher.fullyEnterViewport(function(event, element) {
+          element.watchItem.querySelector('.overlay').style.opacity = 1;
+          element.watchItem.querySelector('.overlay').style.backgroundColor = 'rgba(249, 35, 0, 0.28)';
+        });
+        cardsWatcher.partiallyExitViewport(function(event, element) {
+          element.watchItem.querySelector('.overlay').style.opacity = 0;
+          element.watchItem.querySelector('.overlay').style.backgroundColor = 'rgba(249, 35, 0, 0.28)';
+        });
       });
-      cardsWatcher.partiallyExitViewport(function(event, element) {
-        element.watchItem.querySelector('.overlay').style.opacity = 0;
-        element.watchItem.querySelector('.overlay').style.backgroundColor = 'rgba(249, 35, 0, 0.28)';
-      });
-    });
-
+    }
   });
 })();
